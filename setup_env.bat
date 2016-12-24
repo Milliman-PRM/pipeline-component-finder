6333c23bd5e47302539a5d0d9669bfa8f002a4cf
Sheamus K. Parkes <shea.parkes@milliman.com>
2016-12-24 13:41:57 -0500

    Author very basic setup_env.bat.
    Do this so it can be ran nicely from other sources.

---
+++ setup_env.bat
@@ -0,0 +1,16 @@
+rem ### CODE OWNERS: Shea Parkes
+rem
+rem ### OBJECTIVE:
+rem   Setup the environment.
+rem
+rem ### DEVELOPER NOTES:
+rem   Focused on Python for now.
+
+ENABLEDELAYEDEXPANSION
+
+rem ### LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
+
+
+call activate prod2016_11
+set PYTHONPATH=%~dp0/python
+set PYTHONPATH=%PYTHONPATH%;S:\MISC\_IndyMacros\Code\python
