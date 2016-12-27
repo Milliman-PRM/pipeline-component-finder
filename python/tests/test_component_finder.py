86e4e6c68aaa1a734c4a6223f15d13cd29cd4f29
Kyle Baird <kyle.baird@milliman.com>
2016-12-27 13:51:48 -0500

    Co-own the schema structure
    Document my collaboration.

--- python/tests/test_component_finder.py
+++ python/tests/test_component_finder.py
@@ -1,5 +1,5 @@
 """
-### CODE OWNERS: Shea Parkes
+### CODE OWNERS: Shea Parkes, Kyle Baird
 
 ### OBJECTIVE:
   Test the easily testible bits.
--- setup_env.bat
+++ setup_env.bat
@@ -1,4 +1,4 @@
-rem ### CODE OWNERS: Shea Parkes
+rem ### CODE OWNERS: Shea Parkes, Kyle Baird
 rem
 rem ### OBJECTIVE:
 rem   Setup the environment.
