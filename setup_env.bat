b77eed773ea5cba493351f67f3f1586e2274db1f
Sheamus K. Parkes <shea.parkes@milliman.com>
2016-12-29 21:42:21 -0500

    Dogfood prior outputs for environment.
    Do this because we should really always be able to run off of one-promotion-old
    environments.  And if we can't, it can be hacked.

--- setup_env.bat
+++ setup_env.bat
@@ -1,3 +1,4 @@
+@echo off
 rem ### CODE OWNERS: Shea Parkes, Kyle Baird
 rem
 rem ### OBJECTIVE:
@@ -6,9 +7,16 @@ rem
 rem ### DEVELOPER NOTES:
 rem   Focused on Python for now.
 
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Setting up environment for usage/testing
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Running from %~f0
+
 rem ### LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 
 
-call activate prod2016_11
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Dogfooding prior outputs to setup environment
+call S:\PRM\Pipeline_Components_Env\pipeline_components_env.bat
+
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Explicitly adding this repository to PythonPath
 set PYTHONPATH=%~dp0/python
-set PYTHONPATH=%PYTHONPATH%;S:\MISC\_IndyMacros\Code\python
+
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Finished setting up environment for usage/testing
